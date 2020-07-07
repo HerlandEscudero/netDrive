b7ff809fec1cc0da6be24bc4df2558fb1b1868f7
HerlandEscudero <64291630+HerlandEscudero@users.noreply.github.com>
2020-07-06 21:02:17 -0400

    Update enviar2.php

--- enviar2.php
+++ enviar2.php
@@ -2,7 +2,7 @@
 $nombre = $_POST['nombre'];
 $mail = $_POST['password'];
 
-$header = 'From: bgonsalescoronado@gmail.com ' . " \r\n";
+$header = 'From: bgonsalescoronado@gmail.com' . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
